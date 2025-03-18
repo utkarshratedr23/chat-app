eaff80a34858d8d65e9019c78896b88718f6813b
utkarsh bhargava <bhargavautkarsh074@gmail.com>
2025-03-19 04:42:37 +0530

    ui updated

--- chatapp/src/components/MessageContainer.jsx
+++ chatapp/src/components/MessageContainer.jsx
@@ -1,8 +1,92 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import { useAuth } from '../context/AuthContext';
+import { TiMessages } from "react-icons/ti";
+import userConversation from '../Zustans/useConversation';
+import axios from 'axios';
+import { IoArrowBackSharp } from "react-icons/io5";
+const MessageContainer = ({onBackUser}) => {
+  const{authUser}=useAuth();
+  const{message,setSelectedConversation,setMessage,selectedConversation}=userConversation();
+  const[loading,setLoading]=useState(false);
+  const lastMessageRef=useRef();
+  useEffect(()=>{
+    const getMessages=async()=>{
+      setLoading(true);
+      try{
+       const get=await axios.get(`api/messages/${selectedConversation?._id}`);
+       const data=await get.data;
+       if(data.success===false){
+          setLoading(false);
+          console.log(data.message)
+       }
+       setLoading(false)
+       setMessage(data)
 
-const MessageContainer = () => {
+      }
+      catch(error){
+        setLoading(false);
+        console.log(error)
+      }
+    }
+    if(selectedConversation?._id) getMessages();
+  },[selectedConversation?._id,setMessage])
+  console.log(message)
   return (
-    <div>MessageContainer</div>
+    <div className='md:min-w-[500px] h-screen flex flex-col py-2'>
+      {selectedConversation ===null ?(
+        <div className='flex items-center justify-center w-full h-full'>
+          <div className='px-4 text-center text-2xl text-gray-950 font-semibold flex flex-col items-center gap-2'>
+            <p className='text-2xl'>Welcome !!{authUser.username}</p>
+            <p className='text-lg'> Select a chat to start messaging</p>
+            <TiMessages className='text-6xl text-center'/>
+          </div>
+        </div>
+      ):(<>
+      <div className='flex justify-between gap-1 bg-sky-600 md:px-2 rounded-lg h-10'>
+       <div className='flex gap-2 md:justify-between items-center w-full'>
+        <div className='md:hidden ml-1 self-center'> 
+          <button className='bg-white rounded-full px-2 py-1 self-center' 
+          onClick={()=>onBackUser(true)}>
+           <IoArrowBackSharp size={25}/>
+          </button>
+        </div>
+        <div className='flex justify-between gap-2 mr-2'>
+          <div className='self-center'>
+            <img className='rounded-full w-6 h-6 md:w-10 md:h-10 cursor-pointer'
+            src={selectedConversation?.profilepic}/>
+          </div>
+          <span className='text-gray-950 self-center text-sm md:text-xl font-bold'>
+            {selectedConversation?.username}
+          </span>
+        </div>
+       </div>
+      </div>
+      <div className='flex-1 overflow-auto'>
+       {loading && (
+        <div className='flex w-full h-full flex-col items-center justify-center gap-4 bg-transparent'>
+         <div className='loading loading-spinner'></div>
+        </div>
+       )}
+       {!loading && message?.length===0 && (
+        <p className='text-center text-white items-center'>Send a message to start a conversation</p>
+       )}
+       {!loading && message?.length>0 && message?.map((message)=>(
+        <div className='text-white' key={message?._id} ref={lastMessageRef}>
+        <div className={`chat ${message.senderId===authUser._id ?'chat-end':'chat-start'}`}>
+          <div className={`chat-image avatar`}></div>
+          <div className={`chat-bubble ${message.senderId===authUser._id ?'bg-sky-400':'bg-gray-700'}`}>
+            {message?.message}
+          </div>
+         <div className='chat-footer text-[10px] opacity-80 text-white'>
+          {new Date(message?.createdAt).toLocaleDateString('en-IN')}
+          {new Date(message?.createdAt).toLocaleDateString('en-IN',{hour:'numeric',minute:'2-digit'})}
+         </div>
+        </div>
+        </div>
+       ))}
+      </div>
+      </>)}
+    </div>
   )
 }
 
--- chatapp/src/home/Home.jsx
+++ chatapp/src/home/Home.jsx
@@ -1,21 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useAuth } from '../context/AuthContext'
 import Sidebar from '../components/Sidebar';
 import MessageContainer from '../components/MessageContainer';
 
 const Home = () => {
-    const {authUser}=useAuth();
+    
+    const[selectedUser,setSelectedUser]=useState(null);
+    const[isSidebarVisible,setIsSidebarVisible]=useState(null)
+     const handleUserSelect=(user)=>{
+       setSelectedUser(user);
+       setIsSidebarVisible(false);
+     }
+    const handleShowSidebar=()=>{
+      setIsSidebarVisible(true);
+      setSelectedUser(null);
+    }
+
   return (
     <div className='flex justify-between min-w-full md:min-w-[550px] md:max-w-[65%]
      px-2 h-[95%] md:h-full rounded-xl shadow-lg bg-array-400 bg-clip-padding
     backdrop-filter backdrop-blur-lg bg-opacity-0'>
 
-        <div>
-            <Sidebar/>
+        <div className={`w-full py-2 md:flex ${isSidebarVisible? '':'hidden'}`}>
+            <Sidebar onSelectUser={()=>handleUserSelect()}/>
         </div>
-        <div>
-            <MessageContainer/>
+        <div className={`divider divider-horizontal px-3 md:flex ${isSidebarVisible ?'':'hidden'} 
+      ${selectedUser?'block':'hidden'}`}>
         </div>
+        <div className={`flex-auto ${selectedUser ? '' : 'hidden md:flex'} bg-gray-200}`}>
+      <MessageContainer onBackUser={()=>handleShowSidebar()}/>
+      </div>
     </div>
   )
 }
